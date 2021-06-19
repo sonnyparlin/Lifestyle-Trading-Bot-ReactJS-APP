e1199e1213d9e50564da7c1979d80a778cffddc5
Sonny Parlin <sonnyparlin@gmail.com>
2021-06-19 02:28:53 -0400

    Moved things back to function components

--- src/App.js
+++ src/App.js
@@ -1,73 +1,55 @@
 // client/src/App.js
 import ReactJson from 'react-json-view';
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Form from "./Form"
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
+export default function App() {
 
-    this.state = {
-      investment: '1000',
-      years: '10',
-      pocket: '0.25',
-      strategy: {}
+  const [investment, setInvestment] = useState('1000');
+  const [years, setYears] = useState('10');
+  const [pocket, setPocket] = useState('0.25');
+  const [strategy, setStrategy] = useState({});
+
+  let url = 'https://ltbc.herokuapp.com/api?'
+  url += `investment=${investment}&years=${years}&yearlypocket=${pocket}`
+
+  useEffect(() => {
+    if (investment) {
+      fetch(url).then(response => response.json())
+        .then(data => setStrategy({strategy: JSON.parse(data)}));
     }
+  });
 
-    this.handleClick = this.handleClick.bind(this)
-    this.handleInvestmentChange = this.handleInvestmentChange.bind(this)
-    this.handleYearsChange = this.handleYearsChange.bind(this)
-    this.handlePocketChange = this.handlePocketChange.bind(this)
-  }
-
-  handleInvestmentChange(e) {
-    this.setState({investment:e.target.value})
-  }
-  handleYearsChange(e) {
-    this.setState({years:e.target.value})
-  }
-  handlePocketChange(e) {
-    this.setState({pocket:e.target.value})
-  }
-
-  handleClick(e) {
+  const handleClick = (e) => {
+    setStrategy({})
     e.preventDefault();
-    let url = 'https://ltbc.herokuapp.com/api?'
-    url += `investment=${this.state.investment}&years=${this.state.years}&yearlypocket=${this.state.pocket}`
-
-    fetch(url).then(response => response.json())
-    .then(data => this.setState({strategy: JSON.parse(data)}));
+    setInvestment(document.getElementById('investment').value)
+    setYears(document.getElementById('years').value)
+    setPocket(document.getElementById('pocket').value)
   }
 
-  render() {
+  if('strategy' in strategy) {
     return (
       <div className="App">
         <h1>Lifestyle Trading Bot Strategy</h1>
-        <form>
-          <p>Enter numbers without commas (,) or dollar signs ($). Yearly Pocket is how much you plan to take out of your fund each year, it defaults to 25% (0.25), any number between 0 and 1 is acceptable (for 30% you would enter 0.30). Check out the Lifestyle Trading Bot bot <a href="https://t.me/LifeStyleTrading_Bot?start=1wgcys1037">here</a>.</p>
-          
-          <div className="formFields">
-          <label>
-            Investment (USD):
-          </label>
-          <input onChange={this.handleInvestmentChange} className="investment" type="text" id="investment" name="investment" /><br/>
-          <label>
-            Years (USD):
-          </label>
-            <input onChange={this.handleYearsChange} className="years" type="text" id="years" name="years" /><br/>
-          
-          <label>
-            Yearly Pocket:
-          </label>
-            <input onChange={this.handlePocketChange} className="pocket" type="text" placeholder="0.25" name="pocket" />
-          </div>
-            
-          <button onClick={this.handleClick}>Submit</button>
-        </form>
-        <ReactJson src={this.state.strategy} />
+        <Form investment={investment}
+            years={years}
+            pocket={pocket} 
+          onClick={handleClick} />
+        <ReactJson src={strategy} />
       </div>
     )
   }
-}
 
-export default App;
+  return (
+    <div className="App">
+      <h1>Lifestyle Trading Bot Strategy</h1>
+      <Form investment={investment}
+            years={years}
+            pocket={pocket} 
+          onClick={handleClick} />
+      Loading...
+    </div>
+  )
+}
---
+++ src/Form.js
@@ -0,0 +1,29 @@
+// client/src/App.js
+import React from "react";
+import "./App.css";
+
+export default function Form(props) {
+    return (
+        <form>
+        <p>Enter numbers without commas (,) or dollar signs ($). Yearly Pocket is how much you plan to take out of your fund each year, it defaults to 25% (0.25), any number between 0 and 1 is acceptable (for 30% you would enter 0.30). Check out the Lifestyle Trading Bot bot <a href="https://t.me/LifeStyleTrading_Bot?start=1wgcys1037">here</a>.</p>
+        
+        <div className="formFields">
+        <label>
+          Investment (USD):
+        </label>
+        <input className="investment" defaultValue={props.investment} type="text" id="investment" name="investment" /><br/>
+        <label>
+          Years (USD):
+        </label>
+          <input className="years" defaultValue={props.years} type="text" id="years" name="years" /><br/>
+        
+        <label>
+          Yearly Pocket:
+        </label>
+          <input className="pocket" defaultValue={props.pocket} type="text" id="pocket" placeholder="0.25" name="pocket" />
+        </div>
+          
+        <button onClick={props.handleClick}>Submit</button>
+      </form>
+    )
+}
